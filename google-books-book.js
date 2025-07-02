cc77d4b8a3ec92baa6076d3a9d86cfaac619d2d7
MauriceRivers <122892475+MauriceRivers@users.noreply.github.com>
2025-07-01 22:04:46 -0400

    Add files via upload
    Milestone2

---
+++ google-books-book.js
@@ -0,0 +1,16 @@
+{
+  "kind": "books#volume",
+  "id": "Wfan6L9RGgYC",
+  "volumeInfo": {
+    "title": "Modern Web Development",
+    "authors": ["Dmitry Sheiko"],
+    "publisher": "Apress",
+    "publishedDate": "2015-12-30",
+    "description": "A practical book for building modern web apps with HTML5, CSS3, and JavaScript.",
+    "pageCount": 250,
+    "imageLinks": {
+      "thumbnail": "https://books.google.com/books/content?id=Wfan6L9RGgYC&printsec=frontcover&img=1&zoom=1"
+    },
+    "previewLink": "https://books.google.com/books?id=Wfan6L9RGgYC&dq=modern+web&hl=&cd=1&source=gbs_api"
+  }
+}
---
+++ google-books-search.js
@@ -0,0 +1,29 @@
+{
+  "kind": "books#volumes",
+  "items": [
+    {
+      "volumeInfo": {
+        "title": "Learning Web Design",
+        "authors": ["Jennifer Robbins"],
+        "publisher": "O'Reilly Media",
+        "publishedDate": "2018-05-11",
+        "imageLinks": {
+          "thumbnail": "https://books.google.com/books/content?id=O4W5DwAAQBAJ&printsec=frontcover&img=1&zoom=1"
+        },
+        "previewLink": "https://books.google.com/books?id=O4W5DwAAQBAJ"
+      }
+    },
+    {
+      "volumeInfo": {
+        "title": "HTML and CSS: Design and Build Websites",
+        "authors": ["Jon Duckett"],
+        "publisher": "Wiley",
+        "publishedDate": "2011-11-08",
+        "imageLinks": {
+          "thumbnail": "https://books.google.com/books/content?id=UBFTAQAAQBAJ&printsec=frontcover&img=1&zoom=1"
+        },
+        "previewLink": "https://books.google.com/books?id=UBFTAQAAQBAJ"
+      }
+    }
+  ]
+}
